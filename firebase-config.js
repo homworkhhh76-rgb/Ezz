8d4dd5d4f44c5f4249a51f57a97f2e314192b363
homworkhhh76-rgb <homworkhhh76@gmail.com>
2026-05-04 00:26:20 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBcjhJRJdN3M_WXRXrpkYhO3Zks-tUcBMs",
+    authDomain: "cach-59a79.firebaseapp.com",
+    databaseURL: "https://cach-59a79-default-rtdb.firebaseio.com",
+    projectId: "cach-59a79",
+    storageBucket: "cach-59a79.firebasestorage.app",
+    messagingSenderId: "1004437039670",
+    appId: "1:1004437039670:web:1e7211caa0209c2fe14697",
+    measurementId: "G-YFW7XZKLBW"
+  },
+
+  firebaseRoot: "cashier_units_pro_cach_59a79_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_cach_59a79_885766842"
+  ]
+};
